093a34049e00f3d3ecc54bdad8d4edff98578d56
ginkgo <alexvid33@gmail.com>
2022-03-09 10:32:16 +0100

    final product

--- JS/accion.js
+++ JS/accion.js
@@ -8,38 +8,21 @@ async function fetchMoviesJSON(){
 
 fetchMoviesJSON().then(movies => {
     //console.log(movies);
-    for (let index = 0; index < movies.peliculas.length; index++) { //peliculas es el titulo del JSON
+    for (let index = 0; index < movies.peliculas.length; index++) {
         //console.log(movies.peliculas[index]);
-        //console.log(movies.peliculas[index].name);
         let title = movies.peliculas[index].name;
         let img = movies.peliculas[index].image;
         let alt = movies.peliculas[index].altImg;
         let rate=movies.peliculas[index].rating;
         moviesDisplay.innerHTML += `
             <div class="container">
-                  <img class="posters" src="${img}" alt="${alt}">
-                  <p class="movieTitle">${title}</p>
+                <img class="posters" src="${img}" alt="${alt}">
+                <p class="movieTitle">${title}</p>
                 <div class="overlay">
-                  <img src="./img/SVG/star.svg">
-                  <p>${rate}</p>
+                    <img src="./img/SVG/star.svg">
+                    <p>${rate}</p>
                 </div>
             </div>
     `
     }
 });
-
-//no hereda a las nietas, las hijas las pilla bien... 
-
-/*<div>
-    <img class="posters" src="${img}" alt="${alt}">
-    <p class="movieTitle">${title}</p>
-  </div> */
-
-/*<div class="container">
-      <img class="posters" src="${img}" alt="${alt}">
-      <p class="movieTitle">${title}</p>
-    <div class="overlay">
-      <img src="./img/star.svg">
-      <p>${rate}</p>
-    </div>
-  </div> */
