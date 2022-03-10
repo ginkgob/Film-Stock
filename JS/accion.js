7bf37c5e4f9443fd44d5eb69a9ea548fa512a17f
ginkgo <alexvid33@gmail.com>
2022-03-10 14:22:53 +0100

    need to add css to popup

--- JS/accion.js
+++ JS/accion.js
@@ -1,5 +1,5 @@
 
-const requestURL = "https://ginkgob.github.io/Film-Stock/";
+const requestURL = "../JSON/movies.json";
 
 async function fetchMoviesJSON(){
     const response = await fetch(requestURL);
